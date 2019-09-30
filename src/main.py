8dc2d198b87e2abde139165be4b890f307cb528a
smalla <smalla@jungehaie.com>
2019-09-30 15:43:10 +0200

    have to tweak params now, ccf and acf seems to work but acf drops to 0 with lag=1 when initial condition is 0

--- src/main.py
+++ src/main.py
@@ -14,10 +14,11 @@ T = 1 # delay
 R = 1000 # resolution
 T_cycles = 2
 t = np.linspace(0, T_cycles, R) # run simulation for 2 noise cycles
-tau1 = 0.3
-tau2 = 0.3
-e = 0
-initial_condition = 5
+tau = 0.3
+tau1 = tau
+tau2 = tau
+e = 0.5
+initial_condition = 0
 
 noise = white_noise
 
@@ -33,11 +34,12 @@ ou2 = mixed_noise_ou(t, noise1, noise2, R, T_cycles, e, tau2, initial_condition)
 
 plt_ou(t, ou1, ou2)
 
-lags = 1000
+lags = 20
 plt_acf(acf(ou1, nlags=lags), acf(ou2, nlags=lags))
 # my_ccf = [np.correlate(ou1, np.roll(ou2, int(t))) for t in np.arange(round(R/2-5), round(R/2+5), 1)]
-w = 500
+w = 5
 shifts = np.arange(round(R / 2 - w), round(R / 2 + w), 1)
+t1 = round(R / T_cycles)
 my_ccf = normalized_correlation(ou1, ou2, shifts)
 
 fig = plt.figure()
